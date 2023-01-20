fe6d1bca61e0fcd21ff0154d6b4f7a4fc1c35aae
Dre <connect2andre@hotmail.com>
2023-01-20 19:43:33 +0000

    time tracking function added

--- assets/script.js
+++ assets/script.js
@@ -14,5 +14,31 @@ $(document).ready(function() {
         localStorage.setItem(time, text);
     })
 
-   
+    function Tracker() {
+
+        // Code to get the current hour:
+        var currentTime = moment().hour();
+
+        // Code that loops over each time blocks:
+        $(".time-block").each(function() {
+            var blockTime = parseInt($(this).attr("id").split("clock")[1]);
+
+            // Code that adds the background colours depending on the time:
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    Tracker();
 })
